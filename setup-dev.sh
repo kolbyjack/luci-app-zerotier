fcde88b26832ec3047e3a0ccb00f1fb572306300
Jonathan Kolb <kolbyjack@gmail.com>
2021-11-27 01:29:51 +0000

    Use symlinks for development instead of copying

---
+++ setup-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+function die() {
+    echo "$@"
+    exit 1
+}
+
+function linkdir() {
+    local src="$1"
+    local dst="$2"
+
+    (
+        [ "${src:0:1}" != "/" ] && src="$PWD/$src"
+        [ "${dst:0:1}" != "/" ] && die "Destination must be absolute path"
+
+        cd "$src"
+        find * -type f | while read f; do
+            ln -sf "$src/$f" "$dst/$f"
+        done
+    )
+}
+
+uci set luci.ccache.enable=0
+uci commit luci
+
+linkdir applications/luci-app-zerotier/htdocs /www
+linkdir applications/luci-app-zerotier/root /
