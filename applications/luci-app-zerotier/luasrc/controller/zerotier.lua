43f87aab61830a37bfefa488feab8a5e83fb3c8f
Jonathan Kolb <kolbyjack@gmail.com>
2021-12-02 13:39:35 -0500

    Move app to /admin/vpn

--- applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+++ applications/luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -1,7 +1,7 @@
 module("luci.controller.zerotier", package.seeall)
 function index()
     if nixio.fs.access("/etc/config/zerotier") then
-        local e = entry({"admin", "network", "zerotier"}, cbi("zerotier"), _("ZeroTier"))
+        local e = entry({"admin", "vpn", "zerotier"}, cbi("zerotier"), _("ZeroTier"))
         e.acl_depends = { "luci-app-zerotier" }
     end
 end
