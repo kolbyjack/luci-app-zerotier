2fbe1ce44e37501b5cc80187d2e49a21f85b3d02
Jonathan Kolb <kolbyjack@gmail.com>
2021-12-30 12:37:32 -0500

    Switch back to js view

--- applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.zerotier", package.seeall)
-function index()
-    if nixio.fs.access("/etc/config/zerotier") then
-        local e = entry({"admin", "vpn", "zerotier"}, cbi("zerotier"), _("ZeroTier"))
-        e.acl_depends = { "luci-app-zerotier" }
-    end
-end
