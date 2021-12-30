a3c66f11883243036e82ba0770ccd7b6cc138136
Jonathan Kolb <kolbyjack@gmail.com>
2021-12-30 12:38:40 -0500

    Improve setup-dev script

--- setup-dev.sh
+++ setup-dev.sh
@@ -9,12 +9,15 @@ function linkdir() {
     local src="$1"
     local dst="$2"
 
+    [[ -d "$src" ]] || return
+
     (
         [ "${src:0:1}" != "/" ] && src="$PWD/$src"
         [ "${dst:0:1}" != "/" ] && die "Destination must be absolute path"
 
         cd "$src"
         find * -type f | while read f; do
+            mkdir -p "$( dirname "$dst/$f" )"
             ln -sf "$src/$f" "$dst/$f"
         done
     )
@@ -23,6 +26,10 @@ function linkdir() {
 uci set luci.ccache.enable=0
 uci commit luci
 
-#linkdir applications/luci-app-zerotier/htdocs /www
+find /www -type l ! -exec test -e {} \; -print0 | xargs -r -0 rm
+find /usr/share -type l ! -exec test -e {} \; -print0 | xargs -r -0 rm
+find /usr/lib/lua/luci -type l ! -exec test -e {} \; -print0 | xargs -r -0 rm
+
+linkdir applications/luci-app-zerotier/htdocs /www
 linkdir applications/luci-app-zerotier/root /
 linkdir applications/luci-app-zerotier/luasrc /usr/lib/lua/luci
