f213b71a2a8f8bd2e93db37a7eacc54d9c1498f1
Jonathan Kolb <kolbyjack@gmail.com>
2022-01-02 21:51:24 -0500

    Fix target names in ci-build.sh

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -8,5 +8,5 @@ echo "src-link local $CI_PROJECT_DIR" >> feeds.conf.default
 ./scripts/feeds update -a
 make defconfig
 ./scripts/feeds install luci-app-zerotier
-make -o luci-base -o zerotier package/luci-app-zerotier/compile
+make -o feeds/luci/modules/luci-base -o feeds/packages/net/zerotier package/luci-app-zerotier/compile
 mv bin/packages/*/local/*.ipk "$CI_PROJECT_DIR"
