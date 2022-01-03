d87fb0248b9ecf304af3545e49d79555b3c3a6b2
Jonathan Kolb <kolbyjack@gmail.com>
2022-01-02 21:17:21 -0500

    Publish pages

---
+++ tools/ci-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+cd
+git clone git://git.yoctoproject.org/opkg-utils
+
+cd openwrt
+echo "src-link local $CI_PROJECT_DIR" >> feeds.conf.default
+./scripts/feeds update -a
+make defconfig
+./scripts/feeds install luci-app-zerotier
+make package/luci-app-zerotier/compile
+mv bin/packages/*/local/*.ipk "$CI_PROJECT_DIR"
---
+++ tools/ci-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+git clone git://git.yoctoproject.org/opkg-utils
+
+opkg-utils/opkg-make-index . > Packages
+gzip < Packages > Packages.gz
+
+FILE_LIST=""
+for p in Packages Packages.gz *.ipk; do
+  FILE_LIST="$FILE_LIST<a href=\"$p\">$p</a><br/>"
+done
+
+mkdir -p public
+
+cat > public/index.html <<-EOF
+<html>
+<head>
+  <title>luci-app-zerotier packages</title>
+</head>
+<body>
+  $FILE_LIST
+</body>
+</html>
+EOF
